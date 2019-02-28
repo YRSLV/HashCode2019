e7e6e69e8dbe70f787be0bde7f8462b7f8745b50
Alex Kharchenko <doom9936.1999@gmail.com>
2019-02-28 21:32:26 +0200

    add data check

--- index.php
+++ index.php
@@ -6,9 +6,31 @@
  * Time: 8:10 PM
  */
 
-$tasks = array("a_example.txt", "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt", "e_shiny_selfies.txt");
+$tasks = array("a_example.txt"/*, "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt", "e_shiny_selfies.txt"*/);
 foreach($tasks as $task) {
-    $task_file = fopen($tasks, "r+") or die("Unable to open file!");
+    $task_file = fopen("tasks/" . $task, "r+") or die("Unable to open file!");
+    $task_array = array();
+    // $task_text = fgets($task_file, 1024);
+    while (($buffer = fgets($task_file)) !== false) {
+        array_push($task_array, $buffer);
+    }
+    // $task_text = file_get_contents("tasks/" . $task);
+    $iteration = 0;
+    $amount_of_photos = 0;
+    foreach($task_array as $task_array_element) {
+        if($iteration === 0) {
+            $amount_of_photos = intval($task_array);
+            $iteration++;
+        } else {
+            $task_array_element = explode(" ", $task_array_element, 3);
+            $task_each["orientation"] = $task_array_element[0];
+            $task_each["amount_of_tags"] = $task_array_element[1];
+            $task_each["tags"] = $task_array_element[2];
+            $task_each["tags"] = explode(" ", $task_each["tags"]);
+            var_dump($task_each);
+            echo "<br />";
+        }
+    }
     $submission = fopen("submition.txt", "w+") or die("Unable to open file!");
     $txt = "3\n0\n3\n1 2\n";
     fwrite($submission, $txt);
