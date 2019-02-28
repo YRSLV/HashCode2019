ad3818fe8bcb1adcd8b93daf541f85c17314c568
Yaroslav <yar.re.slav@gmail.com>
2019-02-28 20:20:28 +0200

    Rewrite index.php

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
  * Time: 8:10 PM
  */
 
-echo "4
+$submission = fopen("sub.txt", "w") or die("Unable to open file!");
+$txt = "4
 H 3 cat beach sun
 V 2 selfie smile
 V 2 garden selfie
 H 2 garden cat";
+fwrite($submission, $txt);
+fclose($submission);
