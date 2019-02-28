44ddbbd074a590d961c75393f512a5281a611692
Alex Kharchenko <doom9936.1999@gmail.com>
2019-02-28 23:44:36 +0200

    slide creation

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
  */
 
 $tasks = array("a_example.txt"/*, "b_lovely_landscapes.txt", "c_memorable_moments.txt", "d_pet_pictures.txt", "e_shiny_selfies.txt"*/);
-$elements = [];
-$tag_num = [];
+// $task_H_elements = array();
+// $task_V_elements = array();
+$task_elements = array();
+$tag_num = array();
+$slides = array();
+$amount_of_slides = 0;
+$txt = "";
+
 foreach($tasks as $task) {
-    $task_elements = array();
     $task_file = fopen("tasks/" . $task, "r+") or die("Unable to open file!");
     $task_array = array();
     while (($buffer = fgets($task_file)) !== false) {
@@ -19,17 +24,38 @@ foreach($tasks as $task) {
     $iteration = 0;
     $amount_of_photos = 0;
 
-    function maxVal($arr) {
-        //$max = array_keys($arr, max($arr["amount_of_tags"]));
-        //$max = array_keys($arr, max(array_column($var, "amount_of_tags")));
-        foreach ($arr as $value) {
-            global $tag_num;
-            array_push($tag_num, $value["amount_of_tags"]);
+    function maxVal($arr, $orientation = "H") {
+        $tag_num = array();
+        $max_tags = 0;
+        $max_key = 0;
+        foreach ($arr as $key => $value) {
+            if($value["orientation"] === $orientation) {
+                // array_push($tag_num, $value["amount_of_tags"]);
+                if($value["amount_of_tags"] >= $max_tags) {
+                    $max_tags = $value["amount_of_tags"];
+                    $max_key = $key;
+                }
+            }
         }
-        global $tag_num;
-        $max_tagNum = array_keys($tag_num, max($tag_num));
-        return $max_tagNum;
+        // $max_tagNum = array_keys($tag_num, max($tag_num));
+        // echo $max_tagNum[0] . "<br />";
+        // return $max_tagNum[0];
+        return $key;
     }
+
+    function CheckOrientation($element) {
+        if($element["orientation"] === "H") {
+            return "H";
+        } else {
+            return 1;
+        }
+    }
+
+    function CreateSlide($task_elems, $orientation = "H") {
+        $Slide_id = maxVal($task_elems, $orientation);
+        return $Slide_id;
+    }
+
 
     foreach($task_array as $task_array_element) {
         if($iteration === 0) {
@@ -41,19 +67,63 @@ foreach($tasks as $task) {
             $task_element["amount_of_tags"] = intval($task_array_element[1]);
             $task_element["tags"] = $task_array_element[2];
             $task_element["tags"] = explode(" ", $task_element["tags"]);
-            array_push($elements, $task_element);
-            //var_dump($task_element);
-            //
-            //echo(maxVal($task_element));
-            //echo "<br />";
+            array_push($task_elements, $task_element);
+            /*if($task_element["orientation"] === "H") {
+                array_push($task_H_elements, $task_element);
+            } else {
+                array_push($task_V_elements, $task_element);
+            }*/
         }
     }
 
-    var_dump($elements);
-    var_dump(maxVal($elements));
+    var_dump($task_elements);
+    echo "<br />";
+    echo "<br />";
+    while($task_elements != NULL) {
+        $Slide_id = maxVal($task_elements);
+        unset($task_elements[$Slide_id]);
+        $txt .= $Slide_id . "\n";
+        $amount_of_slides++;
+        var_dump($task_elements);
+        echo "<br />";
+        echo "<br />";
+        $Slide_id = maxVal($task_elements, "V");
+        unset($task_elements[$Slide_id]);
+        $txt .= $Slide_id . " ";
+        $Slide_id = maxVal($task_elements, "V");
+        unset($task_elements[$Slide_id]);
+        $txt .= $Slide_id . "\n";
+        $amount_of_slides++;
+    }
+
+    /*var_dump($task_elements);
+    echo "<br />";
+    $current_max_id = maxVal($task_elements);
+    $current_max = $task_elements[$current_max_id];
+    var_dump($current_max);
+    echo "<br />";
+    echo CheckOrientation($current_max);
+    echo "<br />";
+
+    if(CheckOrientation($current_max) === 1) {
+        $slide_H_id = $current_max_id;
+        unset($array[$current_max_id]);
+        $txt = $current_max_id . "\n";
+        $amount_of_slides++;
+    }// else {
+    //    $slide_V_id[0] = $current_max_id;
+    //}*/
+    
+    /*
+    if($task_elements[maxVal($task_elements)]["orientation"] === "H") {
+        $max_current_Hslide = $task_elements[maxVal($task_elements)];
+    } else {
+        $max_current_firstelement_Vslide = $task_elements[maxVal($task_elements)];
+    }
+    */
 
     $submission = fopen("submition.txt", "w+") or die("Unable to open file!");
-    $txt = "3\n0\n3\n1 2\n";
+    //$txt = "3\n0\n3\n1 2\n";
     fwrite($submission, $txt);
     fclose($submission);
     fclose($task_file);
